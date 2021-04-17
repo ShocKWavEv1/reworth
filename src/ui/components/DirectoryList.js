ec5e10cd5e6d3bc0008ed87f82d27c7cfcb58ad6
ShocKWavEv1 <rodd.sal23@gmail.com>
2021-04-17 17:03:17 -0500

    Search by name

--- src/ui/components/DirectoryList.js
+++ src/ui/components/DirectoryList.js
@@ -1,19 +1,20 @@
-import { Row, Col } from 'react-flexbox-grid';
-import GET_DIRECTORY from '../../settings/api';
-import Card from './CardOffer';
+import { Row, Col } from "react-flexbox-grid";
+import GET_DIRECTORY from "../../settings/api";
+import Card from "./CardOffer";
+import empty from "../assets/empty.svg";
 
 export default function DirectoryList({ ...props }) {
-
     const [perPage, setPerPage] = useState(1);
     const [offerList, setOfferList] = useState([]);
+    const [shadowOfferList, setShadowOfferList] = useState([]);
 
     useEffect(() => {
         fetchOffersList();
     }, []);
 
     useEffect(() => {
-        if(perPage === 1){
-            return undefined
+        if (perPage === 1) {
+            return undefined;
         }
         fetchOffersList();
     }, [perPage]);
@@ -21,35 +22,127 @@ export default function DirectoryList({ ...props }) {
     const fetchOffersList = () => {
         props.setLoader(true);
         GET_DIRECTORY.getDirectory(perPage)
-        .then((data) => {
-            console.log(data);
-            setOfferList(prevOffers => ([...prevOffers, ...data.data]));
-            props.setLoader(false);
-        })
-        .catch((error) => {
-            console.log(error);
-            props.setLoader(false);
-        });
-    }
+            .then(data => {
+                console.log(data);
+                setOfferList(prevOffers => [...prevOffers, ...data.data]);
+                setShadowOfferList(prevOffers => [...prevOffers, ...data.data]);
+                props.setLoader(false);
+            })
+            .catch(error => {
+                console.log(error);
+                props.setLoader(false);
+            });
+    };
 
-    return(
+    const handleFilter = e => {
+        console.log(e.target.value);
+        let updatedArr = offerList;
+        updatedArr = updatedArr.filter((item, i) => {
+            return (
+                item.name.toLowerCase().search(e.target.value.toLowerCase()) !==
+                -1
+            );
+        });
+
+        setShadowOfferList(updatedArr);
+
+        console.log(shadowOfferList);
+    };
+
+    const renderEmptyState = () => {
+        return (
+            <div
+                style={{
+                    padding: "2rem 0rem",
+                    width: "100%",
+                    height: "auto",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                }}>
+                <img src={empty} alt={empty} style={{ width: "50%" }} />
+                <div style={{ marginTop: "-1.15em" }}>
+                    <h1
+                        className="main-title-color"
+                        style={{ fontSize: "2.25rem" }}>
+                        No hay data que mostrar
+                    </h1>
+                </div>
+            </div>
+        );
+    };
+
+    return (
         <>
-            <Row className="no-margin" style={{ width: '100%', height: perPage === 1 && props.isLoading === true ? '100vh' : 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'row', flexFlow: 'wrap' }}>
-                {
-                    offerList.map((item , i) => {
-                        return(
-                            <Col className="no-padding" key={i} xs={12} sm={12} md={6} lg={3} style={{ width: '100%', marginRight: 10, height: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-                                <Card item={item} i={i} />
-                            </Col>
-                        )
-                    })
-                }
+            <Row
+                className="no-margin"
+                style={{
+                    width: "100%",
+                    height:
+                        perPage === 1 && props.isLoading === true
+                            ? "100vh"
+                            : "auto",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row",
+                    flexFlow: "wrap",
+                }}>
+                <div
+                    style={{
+                        width: "100%",
+                        padding: "1rem 0 3rem 0",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+                    <input
+                        className="search-input"
+                        type="text"
+                        placeholder="Busca por nombre"
+                        onChange={e => handleFilter(e)}
+                    />
+                </div>
+                {shadowOfferList.length === 0
+                    ? renderEmptyState()
+                    : shadowOfferList.map((item, i) => {
+                          return (
+                              <Col
+                                  className="no-padding"
+                                  key={i}
+                                  xs={12}
+                                  sm={12}
+                                  md={6}
+                                  lg={3}
+                                  style={{
+                                      width: "100%",
+                                      marginRight: 10,
+                                      height: "auto",
+                                      display: "flex",
+                                      alignItems: "center",
+                                      justifyContent: "center",
+                                      flexDirection: "column",
+                                  }}>
+                                  <Card item={item} i={i} />
+                              </Col>
+                          );
+                      })}
             </Row>
-            <div style={{ marginTop: '2em', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <button className="main-cta" onClick={() => {setPerPage(perPage + 1);}}>
-                    <span className="white-title-color">
-                        Cargar más
-                    </span>
+            <div
+                style={{
+                    marginTop: "2em",
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                <button
+                    className="main-cta"
+                    onClick={() => {
+                        setPerPage(perPage + 1);
+                    }}>
+                    <span className="white-title-color">Cargar más</span>
                 </button>
             </div>
         </>
